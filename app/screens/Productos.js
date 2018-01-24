3d6340d595020ebbcd37206848a2b431dbfa5ee4
Giovanny Avila <gjavila1995@gmail.com>
2018-01-24 23:05:58 +0100

    AGREGADA LA LIBRERIA MOMENT PARA MANEJO DE FECHAS

--- app/screens/Productos.js
+++ app/screens/Productos.js
@@ -13,18 +13,24 @@ import {
 	View,
 	Thumbnail,
 	H2,
-	Spinner
+	Spinner,
+	H3,
+	Left,
+	Button,
+	Right
 } from 'native-base';
 import { 
 	Alert,
 	Dimensions,
 	StatusBar,
-	Image
+	Image,
+	TouchableOpacity
 } from 'react-native';
 
 import Product from '../classes/Product'
 import { store } from '../redux/store';
 import FontAwesome, { Icons } from 'react-native-fontawesome';
+const moment = require('moment')
 
 export default class Productos extends React.Component{
 
@@ -53,27 +59,66 @@ export default class Productos extends React.Component{
 	}
 
 	_renderCol(dato){
+		let day = moment(dato.created_at).format('DD-MM-YYYY');
 		return(
 			<Col style={{width: "100%"}}>
 				<Card>
+					<CardItem>
+						<Body>
+							<H3 style={{fontWeight: 'bold'}}>
+								{ dato.name }
+							</H3>
+							<Text note>
+								{' \n'}
+								<FontAwesome>{Icons.calendar}</FontAwesome> {day}
+							</Text>
+						</Body>
+						<Right>
+							<Button transparent>
+								<Text>
+									<FontAwesome style={{color: "#02A6A4", fontSize: 20}}>{Icons.pencil}</FontAwesome>
+								</Text>
+							</Button>
+						</Right>
+					</CardItem>
 					<CardItem cardBody>
-						<Image
-							source={{uri: 'https://victormartinez.me/wp-content/uploads/2017/08/react-native-portada.jpg'}}
-							style={{ height: 200, width: "100%", flex: 1 }}
-					/>
+							<Image
+								source={{uri: 'https://victormartinez.me/wp-content/uploads/2017/08/react-native-portada.jpg'}}
+								style={{ height: 200, width: "100%", flex: 1 }}
+								/>
+
+							<View
+								style={{
+									position: "absolute",
+									top: 130,
+									bottom: 0,
+									backgroundColor: "#10181B",
+									width: 220,
+									paddingLeft: 9,
+									paddingTop: 3
+								}}
+							>
+							<Text style={{color: "#ffffff", fontWeight: 'bold', marginLeft: 3}} >
+								{dato.description}
+							</Text>
+							</View>
 					</CardItem>
 					<CardItem>
-						<H2 style={{fontWeight: 'bold'}}>
-							{ dato.name }
-						</H2>
-					</CardItem>
-					<CardItem>
-						<Text>
-							<FontAwesome>{Icons.dollar}</FontAwesome> 
-							{dato.price}
-							{'\n'}
-							<FontAwesome>{Icons.calendar}</FontAwesome> {'fecha'}
-						</Text>
+						<Left>
+							<Button transparent>
+								<Text style={{fontWeight: 'bold', fontSize: 15, color: "#02A6A4"}}>
+									$ {dato.price}
+								</Text>
+							</Button>
+						</Left>
+						<Body>
+							<Button transparent>
+								<Text>
+									<FontAwesome style={{fontSize: 20, color: "#02A6A4"}}>{Icons.bookmark}</FontAwesome>
+									<Text style={{fontWeight: 'bold', fontSize: 15, color: "#02A6A4"}}>{' '+dato.category}</Text>
+								</Text>
+							</Button>
+						</Body>
 					</CardItem>
 				</Card>
 			</Col>
